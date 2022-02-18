4a4c125d26da7c5ef11b41c4ceb7e39cfc8c4bbb
mlced <cedric.ngoupande.edu@groupe-gema.com>
2022-02-18 14:55:27 +0100

    update

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\AWSS3;
 use App\Classe\Cart;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,15 +10,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
-{
+{   private $s3;
+    public function __construct(){
+        $this->s3=new AWSS3();
+    }
     /**
      * @Route("/mon-panier", name="cart")
      */
     public function index(Cart $cart): Response
     {
+        $cartFull=$cart->getFull();
+        foreach ($cartFull as $product){
+            $product->product=$this->s3->getS3Url($product->product);
+        }
 
         return $this->render('cart/index.html.twig',[
-            'cart'=> $cart->getFull()
+            'cart'=> $cartFull()
         ]);
     }
 
