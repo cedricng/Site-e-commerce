142febd34f9be5b339acd249a6d3056034c4a4a7
mlced <cedric.ngoupande.edu@groupe-gema.com>
2022-02-18 15:02:50 +0100

    update

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -21,7 +21,7 @@ class CartController extends AbstractController
     {
         $cartFull=$cart->getFull();
         foreach ($cartFull as $product){
-
+            dd($product[0]);
             $product[0]=$this->s3->getS3Url($product[0]);
         }
 
